b7a0f79768909ea12f0e0f846c92d6cb2ebdc8c4
pulathyaseen <57310086+pulathyaseen@users.noreply.github.com>
2019-11-07 15:32:47 +0530

    Update 002-second.py

--- 002-second.py
+++ 002-second.py
@@ -93,3 +93,8 @@
                     'required' : _("E-mail Field is required."),
                 },
             }
+            
+#error while auto complete: style of some things doesn't work
+    venv/lib/python2.7/site-packages/dal_select2/widgets.py   #open this file and delete the line that
+        'admin/js/vendor/jquery/jquery%s.js' % extra, # it occured becouse linking of multiple jquery files.
+    #removing this line will automaticallly repair error
