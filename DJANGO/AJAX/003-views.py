48bd09d1864aa0d7e0fa120b0d4f652021144b01
Muhammed Yaseen P <57310086+pulathyaseen@users.noreply.github.com>
2019-12-12 12:12:41 +0530

    Update 003-views.py

--- DJANGO/AJAX/003-views.py
+++ DJANGO/AJAX/003-views.py
@@ -3,7 +3,9 @@ def get_room_number(request):
     print(pk)
     print("enter")
     if Room.objects.filter(room_type=pk).exists():
-        room = Room.objects.get(room_type=pk,is_deleted=False)
+        room = Room.objects.get(room_type=pk,is_deleted=False
+#         for passing a queryset use list() and values() to take only needed elements--- important cannot pass uuid
+#        subjects = list(Subject.objects.filter(semester=sem,is_deleted=False).values('name','subject_code','semester','subject_type'))
 
         response_data = {
             "status" : "true",
@@ -15,3 +17,10 @@ def get_room_number(request):
             "message" : "Room Number is not exists."
         }
     return HttpResponse(json.dumps(response_data),content_type='application/javascript')
+
+                                
+                                
+                                
+                                
+                                
+                                .
