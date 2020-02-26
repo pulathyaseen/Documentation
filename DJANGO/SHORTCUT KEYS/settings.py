b940c89b4958238ea52fee5091f4b913369118ce
Muhammed Yaseen P <57310086+pulathyaseen@users.noreply.github.com>
2020-02-26 15:48:48 +0530

    Update settings.py

--- DJANGO/SHORTCUT KEYS/settings.py
+++ DJANGO/SHORTCUT KEYS/settings.py
@@ -14,20 +14,8 @@ HOTKEYS = [
         'link': '/'
     },
     {
-        'keys': 'n + p',  # go to patient registration
-        'link': '/app/patients/create-registration'
-    },
-    {
-        'keys': 'n + p + r',  # go to create patient receipt
-        'link': '/app/finance/create-patient-receipt'
-    },
-    {
         'keys': 'n + t ',  # go to create transaction
         'link': '/app/finance/create-transaction'
-    },
-    {
-        'keys': 'r + d',  # go to reffer to dr
-        'link': '/app/doctors/create-refer-doctor'
     },
 ]
 SPECIAL_DISABLED = True
