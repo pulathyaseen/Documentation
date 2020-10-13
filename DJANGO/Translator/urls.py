8757a2b75b6cde73fdc4020827cca34437495fbe
Muhammed Yaseen P <57310086+pulathyaseen@users.noreply.github.com>
2020-10-13 13:35:06 +0530

    Update urls.py

--- DJANGO/Translator/urls.py
+++ DJANGO/Translator/urls.py
@@ -1,4 +1,10 @@
 
+urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+    re_path(r'^switch-language/$', general_views.switch_language, name='switch_language'),
+    ...
+]    
+
 urlpatterns += i18n_patterns(
     path('', include(('web.urls', 'web'), namespace='web')),
     path('app/', general_views.app, name='app'),
